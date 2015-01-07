ff7d303b7cb4321a17d57cc2ca7b5cd39200afbf
Justin <justinharjanto@hotmail.com>
2015-01-07 00:20:43 -0800

    started on working on more get calls

--- Server/routes/index.js
+++ Server/routes/index.js
@@ -6,11 +6,46 @@ router.get('/', function(req, res) {
   res.render('index', { title: 'Express' });
 });
 
-router.get('/alldata', function(req, res) {
+router.get('/universities', function(req, res) {
 	var db = req.db;
-	db.collection('alldata').find().toArray(function (err, items) {
+	db.collection('universities').find().toArray(function (err, items) {
         res.json(items);
     });
+});
+
+router.get('/departments', function(req, res) {
+	var db = req.db;
+	var universityNameReq = req.query.universityName;
+	db.collection('departments').find({universities : [{universityName: universityNameReq}]}).toArray(function (err, items) {
+		res.json(items);
+	});
+});
+
+router.get('/courses', function(req, res) {
+	var db = req.db;
+	var universityNameReq = req.query.universityName;
+	var courseNameReq = req.query.courseName;
+	db.collection('courses').find({university: {name: universityNameReq, courses : [{name : courseNameReq}]}}).toArray(function (err, items) {
+		res.json(items);
+	});
+});
+
+router.get('/comments', function(req, res) {
+	var db = req.db;
+	var universityNameReq = req.query.universityName;
+	var courseNameReq = req.query.courseName;
+	db.collection('comments').find({university: {name : universityNameReq, course : [{name : courseNameReq}]}}).toArray(function (err, items) {
+		res.json(items);
+	});
+});
+
+router.get('/replies', function(req, re) {
+	var db = req.db;
+	var universityNameReq = req.query.universityName;
+	var postIdReq = req.query.postId;
+	db.collection('repiles').find({university: {name : universityNameReq, comments : [{postId : postIdReq}]}}).toArray(function (err, items) {
+		res.json(items);
+	});
 });
 
 module.exports = router;
